cf39349d44539a3b61e8a71d18ddb5aaa1cc6d3d
ThiagoLopes <thiagolopes@protonmail.com>
2017-10-21 16:51:47 -0200

    remove secret key

--- uniso/settings.py
+++ uniso/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h64r51hen#*31aoh6kict6kg30ks636_824$$o6ku9-q#b&x#+'
-
+SECRET_KEY = 'ADD HERE YOUR SECRET KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
