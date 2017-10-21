23e270c3f863e4d694bfe9b93c63e1dc7f28c557
ThiagoLopes <thiagolopes@protonmail.com>
2017-10-21 16:50:59 -0200

    update home and querysets

--- dash/views.py
+++ dash/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from .models import Palestra
 
@@ -8,14 +7,18 @@ from .models import Palestra
 class HomeTemplateView(TemplateView):
     template_name = 'home.html'
 
-    # TODO
-    def qs_speakers(self):
-        pass
+    def qs_palestra(self):
+        speaker_four = Palestra.objects.all()[:8]
+        return speaker_four
+
+    def get_qs_all_days_distinct(self):
+        all_days = Palestra.objects.dates('date', 'day')
+        return all_days
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        speakers_four = Palestra.objects.all()
-        context['speaker_four_list'] = speakers_four
+        context['palestra_list'] = self.qs_palestra()
+        context['all_days'] = self.get_qs_all_days_distinct()
         return context
 
 
