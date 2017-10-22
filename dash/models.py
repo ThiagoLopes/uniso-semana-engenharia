d3f90d37beb855dc6287ef8310af2193fe07c6fe
ThiagoLopes <thiagolopes@protonmail.com>
2017-10-22 18:51:21 -0200

    add Palestrante model and NtoN relation

--- dash/models.py
+++ dash/models.py
@@ -7,27 +7,6 @@ from django_extensions.db.fields import (CreationDateTimeField,
 # Create your models here.
 
 
-class Palestra(models.Model):
-
-    talk_name = models.CharField(
-        _('Nome da palestra'), max_length=45, unique=True)
-    talk_description = models.TextField(
-        _('Descrição da palestra'), max_length=90)
-    local = models.CharField(_('Prédio/Bloco da palestra'),
-                             help_text=_('Exemplo: Bloco X'), max_length=30)
-    room = models.CharField(_('Sala da palestra'), max_length=30)
-    date = models.DateField(_('Data'))
-    hour_init = models.TimeField(_('Início'))
-    hour_end = models.TimeField(_('Término'))
-    number_vacancies = models.SmallIntegerField(_('Vagas'))
-    created = CreationDateTimeField()
-    modification = ModificationDateTimeField()
-    slug = AutoSlugField(populate_from=['talk_name', 'room'])
-
-    def __str__(self):
-        return self.talk_name
-
-
 class Palestrante(models.Model):
 
     speaker_name = models.CharField(_('Nome do palestrante'), max_length=45)
@@ -38,3 +17,40 @@ class Palestrante(models.Model):
 
     def __str__(self):
         return self.speaker_name
+
+
+class Palestra(models.Model):
+
+    talk_name = models.CharField(
+        _('Nome da palestra'), max_length=45, unique=True)
+    talk_description = models.TextField(
+        _('Descrição da palestra'), max_length=90)
+    local = models.CharField(
+        _('Prédio/Bloco da palestra'),
+        help_text=_('Exemplo: Bloco X'), max_length=30)
+    room = models.CharField(
+        _('Sala da palestra'), max_length=30)
+    date = models.DateField(
+        _('Data'))
+    hour_init = models.TimeField(
+        _('Início'))
+    hour_end = models.TimeField(
+        _('Término'))
+    number_vacancies = models.SmallIntegerField(
+        _('Vagas'))
+    created = CreationDateTimeField()
+    modification = ModificationDateTimeField()
+    slug = AutoSlugField(
+        populate_from=['talk_name', 'room'])
+    palestrante = models.ManyToManyField(Palestrante)
+
+    @property
+    def str_all_palestrante(self):
+        return ', '.join([str(p) for p in self.palestrante.all()])
+
+    @property
+    def len_palestrantes(self):
+        return len(self.palestrante.all())
+
+    def __str__(self):
+        return self.talk_name
