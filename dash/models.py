dab08dbf4376152cc85cf8bfa83d2fe3996f9abe
ThiagoLopes <thiagolopes@protonmail.com>
2017-10-30 20:29:12 -0200

    remove imports not used

--- dash/models.py
+++ dash/models.py
@@ -1,6 +1,4 @@
-import os
 from django.db import models
-from django.conf import settings
 from PIL import Image
 from django.utils.translation import gettext_lazy as _
 from django_extensions.db.fields import (CreationDateTimeField,
